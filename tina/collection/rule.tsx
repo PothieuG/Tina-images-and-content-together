8e14d7ee09c2ebce957961509796af809a04bdcb
Gilles Pothieu <gillespothieu@ssw.fr>
2025-06-17 14:13:59 +0200

    Adding missing field (date) to Rule collection

--- tina/collection/rule.tsx
+++ tina/collection/rule.tsx
@@ -3,103 +3,99 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import type { Collection } from 'tinacms';
 
 const RuleCollection: Collection = {
-  label: 'Rules',
-  name: 'rule',
-  path: 'content/rules',
-  format: 'mdx',
-  ui: {
-    router: ({ document }) => {
-      return `/rules/${document._sys.breadcrumbs.join('/')}`;
-    },
-    filename: {
-      showFirst: true,
-      slugify: (values) => {
-        const directoryName = values.title?.toLowerCase().split(" ").join("-");
-        return `${directoryName}/rule`;
-      }
-    },
-  },
-  fields: [
-    {
-      type: 'string',
-      label: 'Title',
-      name: 'title',
-      isTitle: true,
-      required: true,
-    },
-    {
-      type: 'reference',
-      label: 'Author',
-      name: 'author',
-      collections: ['author'],
-      ui: {
-        optionComponent: (
-          props: {
-            name?: string;
-            avatar: string;
-          },
-          _internalSys: { path: string }
-        ) => {
-          const { name, avatar } = props;
-          if (!name) return _internalSys.path;
-
-          return (
-            <p className="flex min-h-8 items-center gap-4">
-              <Avatar>
-                {avatar && (
-                  <AvatarImage
-                    src={avatar}
-                    alt={`${name} Profile`}
-                  />
-                )}
-                <AvatarFallback>
-                  {name
-                    .split(' ')
-                    .map((part) => part[0]?.toUpperCase() || '')
-                    .join('')}
-                </AvatarFallback>
-              </Avatar>
-              {name}
-            </p>
-          );
+    label: 'Rules',
+    name: 'rule',
+    path: 'content/rules',
+    format: 'mdx',
+    ui: {
+        router: ({ document }) => {
+            return `/rules/${document._sys.breadcrumbs.join('/')}`;
         },
-      }
+        filename: {
+            showFirst: true,
+            slugify: (values) => {
+                const directoryName = values.title?.toLowerCase().split(' ').join('-');
+                return `${directoryName}/rule`;
+            },
+        },
     },
-    {
-      type: 'object',
-      label: 'Tags',
-      name: 'tags',
-      list: true,
-      fields: [
+    fields: [
         {
-          type: 'reference',
-          label: 'Tag',
-          name: 'tag',
-          collections: ['tag'],
-          ui: {
-            optionComponent: (
-              props: {
-                name?: string;
-              },
-              _internalSys: { path: string }
-            ) => props.name || _internalSys.path
-          },
+            type: 'string',
+            label: 'Title',
+            name: 'title',
+            isTitle: true,
+            required: true,
         },
-      ],
-      ui: {
-        itemProps: (item) => {
-          return { label: item?.tag };
+        { type: 'datetime', name: 'date', label: 'Date', required: true },
+        {
+            type: 'reference',
+            label: 'Author',
+            name: 'author',
+            collections: ['author'],
+            ui: {
+                optionComponent: (
+                    props: {
+                        name?: string;
+                        avatar: string;
+                    },
+                    _internalSys: { path: string }
+                ) => {
+                    const { name, avatar } = props;
+                    if (!name) return _internalSys.path;
+
+                    return (
+                        <p className='flex min-h-8 items-center gap-4'>
+                            <Avatar>
+                                {avatar && <AvatarImage src={avatar} alt={`${name} Profile`} />}
+                                <AvatarFallback>
+                                    {name
+                                        .split(' ')
+                                        .map((part) => part[0]?.toUpperCase() || '')
+                                        .join('')}
+                                </AvatarFallback>
+                            </Avatar>
+                            {name}
+                        </p>
+                    );
+                },
+            },
         },
-      }
-    },
-    {
-      type: 'rich-text',
-      label: 'Body',
-      name: '_body',
-      templates: [],
-      isBody: true,
-    },
-  ],
+        {
+            type: 'object',
+            label: 'Tags',
+            name: 'tags',
+            list: true,
+            fields: [
+                {
+                    type: 'reference',
+                    label: 'Tag',
+                    name: 'tag',
+                    collections: ['tag'],
+                    ui: {
+                        optionComponent: (
+                            props: {
+                                name?: string;
+                            },
+                            _internalSys: { path: string }
+                        ) => props.name || _internalSys.path,
+                    },
+                },
+            ],
+            ui: {
+                itemProps: (item) => {
+                    return { label: item?.tag };
+                },
+            },
+        },
+        {
+            type: 'rich-text',
+            label: 'Body',
+            name: '_body',
+            templates: [],
+            isBody: true,
+        },
+    ],
 };
 
 export default RuleCollection;
